0f7dc143f1bc29b6e856e6ec868e892314f15f06
mashihara <mashiharahiroyuki@gmail.com>
2018-07-29 11:18:45 +0900

    fix(all):first commit

---
+++ src/main/java/net/tennisbear/www/tenniscrazy/TenniscrazyApplication.java
@@ -0,0 +1,12 @@
+package net.tennisbear.www.tenniscrazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenniscrazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenniscrazyApplication.class, args);
+	}
+}
